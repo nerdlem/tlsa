4144ef00b5859456009c20e139874ea6bd94f6ed
Luis Muñoz <lem@uniregistry.link>
2017-08-27 10:36:30 -0700

    Bail gracefully when bad TSIG material is provided

--- tsigread.go
+++ tsigread.go
@@ -8,30 +8,32 @@ import (
 	"os"
 )
 
-// Read and parse a Bind-formatted key file for use with TSIG.
+// ReadTSIG Read and parse a Bind-formatted key file for use with TSIG.
 func ReadTSIG(fileName string) ([]dns.KEY, error) {
 
 	ret := make([]dns.KEY, 0, 1)
 
 	r, err := os.Open(fileName)
 	if err != nil {
-		return ret, fmt.Errorf("Failed to open TSIG file %s: %s\n",
+		return ret, fmt.Errorf("failed to open TSIG file %s: %s",
 			fileName, err)
 	}
 
 	p := dns.ParseZone(r, "", fileName)
 
 	for t := range p {
-
 		rr := t.RR
+		if rr == nil {
+			return ret, fmt.Errorf("TSIG file %s produced a nil RR", fileName)
+		}
 
 		if rr.Header().Class != dns.ClassINET {
-			return ret, fmt.Errorf("Unexpected class %d in RR from TSIG key file %s: %s\n",
+			return ret, fmt.Errorf("unexpected class %d in RR from TSIG key file %s: %s",
 				rr.Header().Class, fileName, err)
 		}
 
 		if rr.Header().Rrtype != dns.TypeKEY {
-			return ret, fmt.Errorf("Unexpected type %d in RR from TSIG key file %s: %s\n",
+			return ret, fmt.Errorf("unexpected type %d in RR from TSIG key file %s: %s",
 				rr.Header().Rrtype, fileName, err)
 		}
 
