9ca6cb641b22d909153d62af3c1c2f8007805fd7
Luis Muñoz <lem@uniregistry.link>
2017-08-27 22:51:39 -0700

    Implement direct reading of a public key

--- readcert.go
+++ readcert.go
@@ -7,9 +7,14 @@ import (
 	"io/ioutil"
 )
 
-// GetCertificate reads a PEM encoded certificate file and return a
-// x509.Certificate. A suitable error is returned when conditions aren't
-// favorable.
+// GetCertificate reads a PEM encoded certificate file or public key.
+//
+// When fed a PEM encoded certificate, the x509.Certificate object is
+// returned. If fed a public key in PEM format, a pseudo x509.Certificate is
+// returned, with only the public key field populated. This is enough to
+// calculate the TLSA signature. return a x509.Certificate.
+//
+// Suitable errors are returned when conditions aren't favorable.
 func GetCertificate(certificateFile string) (*x509.Certificate, error) {
 	certificateBytes, err := ioutil.ReadFile(certificateFile)
 	if err != nil {
@@ -19,14 +24,31 @@ func GetCertificate(certificateFile string) (*x509.Certificate, error) {
 
 	block, _ := pem.Decode(certificateBytes)
 	if block == nil {
-		return nil, fmt.Errorf("failed to parse PEM block")
+		return nil, fmt.Errorf("failed to parse PEM block on file %s",
+			certificateFile)
 	}
 
-	certificate, err := x509.ParseCertificate(block.Bytes)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse certificate: %s", err)
-	}
+	fmt.Printf("dry-run: file %s is of type %s\n", certificateFile, block.Type)
 
+	var certificate *x509.Certificate
+
+	switch block.Type {
+	case "CERTIFICATE":
+		certificate, err = x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse certificate %s: %s",
+				certificateFile, err)
+		}
+
+	case "PUBLIC KEY":
+		certificate = &x509.Certificate{
+			Raw: nil,
+			RawSubjectPublicKeyInfo: block.Bytes,
+		}
+	default:
+		return nil, fmt.Errorf("cert file %s is of unsupported type %s",
+			certificateFile, block.Type)
+	}
 	return certificate, nil
 }
 
